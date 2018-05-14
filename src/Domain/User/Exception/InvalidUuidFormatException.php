b5d79d0ad1b886964029d5b0c859fd9c096d812f
Sergio Gómez <sergio@uco.es>
2018-05-14 18:10:15 +0200

    Code style fixed

--- src/Domain/User/Exception/InvalidUuidFormatException.php
+++ src/Domain/User/Exception/InvalidUuidFormatException.php
@@ -2,10 +2,17 @@
 
 declare(strict_types=1);
 
-namespace App\Domain\User\Exception;
+/*
+ * This file is part of the `gata` project.
+ *
+ * (c) Aula de Software Libre de la UCO <aulasoftwarelibre@uco.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
+namespace App\Domain\User\Exception;
 
 final class InvalidUuidFormatException extends \DomainException
 {
-
 }
