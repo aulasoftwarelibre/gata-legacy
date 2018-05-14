cf242961c7af08e6109bb5f8afd95bf377f37cb3
Sergio Gómez <sergio@uco.es>
2018-05-14 18:14:45 +0200

    Code style fixed in specs

--- spec/Domain/User/Model/UserIdSpec.php
+++ spec/Domain/User/Model/UserIdSpec.php
@@ -2,12 +2,20 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the `gata` project.
+ *
+ * (c) Aula de Software Libre de la UCO <aulasoftwarelibre@uco.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace spec\App\Domain\User\Model;
 
 use App\Domain\User\Exception\InvalidUuidFormatException;
 use App\Domain\User\Model\UserId;
 use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
 
 final class UserIdSpec extends ObjectBehavior
 {
@@ -15,34 +23,34 @@ final class UserIdSpec extends ObjectBehavior
     const NOT_VALID_UUID = 'e8a68535-3e17-468f-acc3-8a3e0fa04a5';
     const OTHER_UUID = 'e8a68535-3e17-468f-acc3-8a3e0fa04a57';
 
-    function let()
+    public function let()
     {
         $uuid = self::UUID;
 
         $this->beConstructedWith($uuid);
     }
 
-    function it_is_initializable()
+    public function it_is_initializable()
     {
         $this->shouldHaveType(UserId::class);
     }
 
-    function it_can_be_created_from_string(): void
+    public function it_can_be_created_from_string(): void
     {
         $this->id()->shouldBe(self::UUID);
     }
 
-    function it_has_to_be_valid(): void
+    public function it_has_to_be_valid(): void
     {
         $this->shouldThrow(InvalidUuidFormatException::class)->during('__construct', [self::NOT_VALID_UUID]);
     }
 
-    function it_can_be_a_string(): void
+    public function it_can_be_a_string(): void
     {
         $this->__toString()->shouldBe(self::UUID);
     }
 
-    function it_can_be_compared_with_other_user_id(
+    public function it_can_be_compared_with_other_user_id(
         UserId $sameUserId,
         UserId $notSameUserId
     ) {
