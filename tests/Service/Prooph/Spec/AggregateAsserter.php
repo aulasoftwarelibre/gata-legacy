d356892c68b72a43959afdf227f889addb1ea4f9
Sergio Gómez <sergio@uco.es>
2018-05-16 19:07:51 +0200

    Added helper tests classes from lchrusciel/Beery project
    Classes for domain event tests

---
+++ tests/Service/Prooph/Spec/AggregateAsserter.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the `gata` project.
+ *
+ * (c) Aula de Software Libre de la UCO <aulasoftwarelibre@uco.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\Service\Prooph\Spec;
+
+use Prooph\EventSourcing\AggregateChanged;
+use Prooph\EventSourcing\EventStoreIntegration\ClosureAggregateTranslator;
+
+final class AggregateAsserter
+{
+    /** @var ClosureAggregateTranslator */
+    private $closureAggregateTranslator;
+
+    /** @var MessageMatcher */
+    private $messageMatcher;
+
+    public function __construct()
+    {
+        $this->closureAggregateTranslator = new ClosureAggregateTranslator();
+        $this->messageMatcher = new MessageMatcher();
+    }
+
+    public function assertAggregateHasProducedEvent($aggregateRoot, AggregateChanged $event): void
+    {
+        $producedEvents = $this->closureAggregateTranslator->extractPendingStreamEvents($aggregateRoot);
+
+        assert(
+            $this->messageMatcher->isOneOf($event, $producedEvents),
+            'Expected one of the aggregate events to match the provided event.'
+        );
+    }
+}
