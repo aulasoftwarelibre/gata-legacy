78f264d416e70e595119075fa6c2bf9167794917
Sergio Gómez <sergio@uco.es>
2018-05-16 18:55:25 +0200

    Created CommentAdded DomainEvent

---
+++ src/Domain/Comment/Event/CommentAdded.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the `gata` project.
+ *
+ * (c) Aula de Software Libre de la UCO <aulasoftwarelibre@uco.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Domain\Comment\Event;
+
+use App\Domain\Comment\Model\CommentId;
+use App\Domain\Idea\Model\IdeaId;
+use App\Domain\User\Model\UserId;
+use Prooph\EventSourcing\AggregateChanged;
+
+class CommentAdded extends AggregateChanged
+{
+    public static function withData(CommentId $commentId, IdeaId $ideaId, UserId $userId, string $text): self
+    {
+        return self::occur($commentId->id(), ['ideaId' => $ideaId->id(), 'userId' => $userId->id(), 'text' => $text]);
+    }
+
+    public function id(): CommentId
+    {
+        return new CommentId($this->aggregateId());
+    }
+
+    public function ideaId(): IdeaId
+    {
+        return new IdeaId($this->payload()['ideaId']);
+    }
+
+    public function userId(): UserId
+    {
+        return new UserId($this->payload()['userId']);
+    }
+
+    public function text(): string
+    {
+        return $this->payload()['text'];
+    }
+}
