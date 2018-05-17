470dbb0d37479e59677d5e6db496db3bae25de52
Víctor <victormonserratvillatoro@gmail.com>
2018-05-17 16:51:49 +0200

    Adding idea voted domain event

---
+++ spec/Domain/Idea/Event/IdeaVotedSpec.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the `gata` project.
+ *
+ * (c) Aula de Software Libre de la UCO <aulasoftwarelibre@uco.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\App\Domain\Idea\Event;
+
+use App\Domain\Idea\Model\IdeaId;
+use App\Domain\User\Model\UserId;
+use PhpSpec\ObjectBehavior;
+use Prooph\Common\Messaging\DomainEvent;
+
+final class IdeaVotedSpec extends ObjectBehavior
+{
+    public function it_is_a_domain_event(): void
+    {
+        $this->shouldHaveType(DomainEvent::class);
+    }
+
+    public function it_represents_idea_voted_event_occurrence(): void
+    {
+        $this->beConstructedThrough('withData', [
+            new IdeaId('4ab37020-455c-45a3-8f7e-194bfb9fbc0b'),
+            new UserId('0c586173-7676-4a2c-9220-edd223eb458e'),
+        ]);
+
+        $this->ideaId()->shouldBeLike(new IdeaId('4ab37020-455c-45a3-8f7e-194bfb9fbc0b'));
+        $this->userId()->shouldBeLike(new UserId('0c586173-7676-4a2c-9220-edd223eb458e'));
+    }
+}
---
+++ src/Domain/Idea/Event/IdeaVoted.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the `gata` project.
+ *
+ * (c) Aula de Software Libre de la UCO <aulasoftwarelibre@uco.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Domain\Idea\Event;
+
+use App\Domain\Idea\Model\IdeaId;
+use App\Domain\User\Model\UserId;
+use Prooph\EventSourcing\AggregateChanged;
+
+class IdeaVoted extends AggregateChanged
+{
+    public static function withData(IdeaId $ideaId, UserId $userId): self
+    {
+        return self::occur($ideaId->id(), [
+            'userId' => $userId->id(),
+        ]);
+    }
+
+    public function ideaId(): IdeaId
+    {
+        return new IdeaId($this->aggregateId());
+    }
+
+    public function userId(): UserId
+    {
+        return new UserId($this->payload()['userId']);
+    }
+}
