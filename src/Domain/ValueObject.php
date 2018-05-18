c6a806a4edd3e87595bf2219b903c5c2de73a41b
Sergio Gómez <sergio@uco.es>
2018-05-18 17:00:57 +0200

    Added ValueObject interface

---
+++ src/Domain/ValueObject.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the `gata` project.
+ *
+ * (c) Aula de Software Libre de la UCO <aulasoftwarelibre@uco.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Domain;
+
+interface ValueObject
+{
+    public function equals(self $object): bool;
+}
