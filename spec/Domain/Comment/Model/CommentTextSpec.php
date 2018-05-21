4bfe216e451f265c15d88859a5ab16704f6e0c0c
Víctor <victormonserratvillatoro@gmail.com>
2018-05-21 19:58:59 +0200

    Adding copyright to some new files

--- spec/Domain/Comment/Model/CommentTextSpec.php
+++ spec/Domain/Comment/Model/CommentTextSpec.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the `gata` project.
+ *
+ * (c) Aula de Software Libre de la UCO <aulasoftwarelibre@uco.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace spec\App\Domain\Comment\Model;
 
 use App\Domain\Comment\Exception\EmptyCommentTextException;
