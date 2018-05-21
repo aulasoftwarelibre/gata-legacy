c0d311ac4de277d5ceff79c7d50fb531e607eb22
Víctor <victormonserratvillatoro@gmail.com>
2018-05-21 17:01:56 +0200

    User spec refactor

--- spec/Domain/User/Model/UserIdSpec.php
+++ spec/Domain/User/Model/UserIdSpec.php
@@ -15,34 +15,30 @@ namespace spec\App\Domain\User\Model;
 
 use App\Domain\User\Exception\InvalidUserIdFormatException;
 use App\Domain\User\Model\UserId;
+use App\Domain\ValueObject;
 use PhpSpec\ObjectBehavior;
 
 final class UserIdSpec extends ObjectBehavior
 {
     const UUID = 'e8a68535-3e17-468f-acc3-8a3e0fa04a59';
-    const NOT_VALID_UUID = 'e8a68535-3e17-468f-acc3-8a3e0fa04a5';
     const OTHER_UUID = 'e8a68535-3e17-468f-acc3-8a3e0fa04a57';
+    const INVALID_UUID = 'e8a68535-3e17-468f-acc3-8a3e0fa04a5';
 
     public function let(): void
     {
         $this->beConstructedWith(self::UUID);
     }
 
-    public function it_is_initializable(): void
+    public function it_is_a_value_object(): void
     {
-        $this->shouldHaveType(UserId::class);
-    }
-
-    public function it_can_be_created_from_string(): void
-    {
-        $this->id()->shouldBe(self::UUID);
+        $this->shouldImplement(ValueObject::class);
     }
 
     public function it_has_to_be_valid(): void
     {
         $this->shouldThrow(InvalidUserIdFormatException::class)->during(
             '__construct',
-            [self::NOT_VALID_UUID]
+            [self::INVALID_UUID]
         );
     }
 
@@ -51,12 +47,15 @@ final class UserIdSpec extends ObjectBehavior
         $this->__toString()->shouldBe(self::UUID);
     }
 
-    public function it_can_be_compared_with_other_user_id(
-        UserId $sameUserId,
-        UserId $notSameUserId
-    ) {
-        $sameUserId->id()->shouldBeCalled()->willReturn(self::UUID);
-        $notSameUserId->id()->shouldBeCalled()->willReturn(self::OTHER_UUID);
+    public function it_has_an_id(): void
+    {
+        $this->id()->shouldBe(self::UUID);
+    }
+
+    public function it_can_be_compared_with_other_user_id()
+    {
+        $sameUserId = new UserId(self::UUID);
+        $notSameUserId = new UserId(self::OTHER_UUID);
 
         $this->equals($sameUserId)->shouldBe(true);
         $this->equals($notSameUserId)->shouldBe(false);
