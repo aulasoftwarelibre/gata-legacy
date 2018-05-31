0bf91a57acc27c980aaadeaec2c72e792b74580f
Sergio Gómez <sergio@uco.es>
2018-05-31 18:37:39 +0200

    Added idea capacity limited domain event

---
+++ src/Domain/Idea/Event/IdeaCapacityLimited.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the `gata` project.
+ *
+ * (c) Aula de Software Libre de la UCO <aulasoftwarelibre@uco.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Domain\Idea\Event;
+
+use App\Domain\Idea\Model\IdeaId;
+use Prooph\EventSourcing\AggregateChanged;
+
+class IdeaCapacityLimited extends AggregateChanged
+{
+    public static function withData(IdeaId $ideaId, int $limit): self
+    {
+        return self::occur($ideaId->value(), [
+            'limit' => $limit,
+        ]);
+    }
+
+    public function ideaId(): IdeaId
+    {
+        return new IdeaId($this->aggregateId());
+    }
+
+    public function limit(): int
+    {
+        return $this->payload()['limit'];
+    }
+}
