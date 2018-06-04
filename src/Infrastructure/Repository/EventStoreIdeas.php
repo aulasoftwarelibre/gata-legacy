7c4e6a0d574300683e3a5c96098c2429d9f199e1
Víctor Monserrat Villatoro <victormonserratvillatoro@gmail.com>
2018-06-04 18:55:14 +0200

    Uploaded ideas event store service

---
+++ src/Infrastructure/Repository/EventStoreIdeas.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the `gata` project.
+ *
+ * (c) Aula de Software Libre de la UCO <aulasoftwarelibre@uco.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Infrastructure\Repository;
+
+use App\Application\Idea\Exception\IdeaNotFoundException;
+use App\Application\Idea\Repository\Ideas;
+use App\Domain\Idea\Model\Idea;
+use App\Domain\Idea\Model\IdeaId;
+use Prooph\EventSourcing\Aggregate\AggregateRepository;
+
+final class EventStoreIdeas extends AggregateRepository implements Ideas
+{
+    public function save(Idea $idea): void
+    {
+        $this->saveAggregateRoot($idea);
+    }
+
+    public function get(IdeaId $ideaId): ?Idea
+    {
+        $idea = $this->getAggregateRoot($ideaId->value());
+
+        if (!$idea instanceof Idea) {
+            throw new IdeaNotFoundException();
+        }
+
+        return $idea;
+    }
+}
