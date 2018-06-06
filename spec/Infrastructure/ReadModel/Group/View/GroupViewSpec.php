69998b5db03a50e29a513c195759acb492ce3973
Sergio Gómez <sergio@uco.es>
2018-06-06 17:36:52 +0200

    Added group view

---
+++ spec/Infrastructure/ReadModel/Group/View/GroupViewSpec.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the `gata` project.
+ *
+ * (c) Aula de Software Libre de la UCO <aulasoftwarelibre@uco.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\App\Infrastructure\ReadModel\Group\View;
+
+use PhpSpec\ObjectBehavior;
+
+final class GroupViewSpec extends ObjectBehavior
+{
+    const UUID = 'e8a68535-3e17-468f-acc3-8a3e0fa04a59';
+
+    public function let(): void
+    {
+        $this->beConstructedWith(self::UUID, 'Name');
+    }
+
+    public function it_is_simplified_group_view(): void
+    {
+        $this->id()->shouldReturn(self::UUID);
+        $this->name()->shouldReturn('Name');
+    }
+
+    public function it_can_be_renamed(): void
+    {
+        $this->rename('Other name');
+
+        $this->name()->shouldReturn('Other name');
+    }
+}
